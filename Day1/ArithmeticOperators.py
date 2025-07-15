432bfefc89e7105f1f63c9d1c12760112af2ea82
Dev-Archana <asm43636@gmail.com>
2025-07-15 19:54:12 +0530

    day-1

---
+++ Day1/ArithmeticOperators.py
@@ -0,0 +1,9 @@
+# Arithmetic Operators in Python
+# This code performs basic arithmetic operations: addition, subtraction, and multiplication.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ Day1/Division.py
@@ -0,0 +1,7 @@
+# Division.py
+# This code performs integer and float division of two numbers.
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
